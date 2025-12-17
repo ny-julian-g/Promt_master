6cf018bd0227e71eb7ada8ed8982a1eeb5d03872
ny-julian-g <julian.gasser@noseryoung.ch>
2025-12-17 11:54:25 +0100

    actual data in firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-export const app = initializeApp({
-  apiKey: "DEIN_API_KEY",
-  authDomain: "DEINE_DOMAIN",
-  projectId: "DEIN_PROJECT",
-  storageBucket: "DEIN_BUCKET",
-  messagingSenderId: "DEIN_ID",
-  appId: "DEINE_APP_ID"
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyB2rnjkUqZ7zsya8A4-NXjuu8_V2kmJRfQ",
+  authDomain: "promt-master1234.firebaseapp.com",
+  projectId: "promt-master1234",
+  storageBucket: "promt-master1234.firebasestorage.app",
+  messagingSenderId: "77863236164",
+  appId: "1:77863236164:web:754e9cf673890514a68211",
+  measurementId: "G-DV5YXT3QC2"
+};
 
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
