c833a3a92f12efb05a7e6bb58e7be6f554781082
Diego De Corso <179591332+dgdecorso@users.noreply.github.com>
2025-12-18 10:36:33 +0100

    fix js

---
+++ public/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2rnjkUqZ7zsya8A4-NXjuu8_V2kmJRfQ",
+  authDomain: "promt-master1234.firebaseapp.com",
+  projectId: "promt-master1234",
+  storageBucket: "promt-master1234.firebasestorage.app",
+  messagingSenderId: "77863236164",
+  appId: "1:77863236164:web:754e9cf673890514a68211",
+  measurementId: "G-DV5YXT3QC2"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
