b81cf49d8e14470a5e14eeaba116a2df061dd4e8
Diego De Corso <179591332+dgdecorso@users.noreply.github.com>
2025-12-19 09:51:46 +0100

    fix png upload 3

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,18 @@
-
 import { initializeApp } from
-"https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+        "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from
-"https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+        "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2rnjkUqZ7zsya8A4-NXjuu8_V2kmJRfQ",
-  authDomain: "promt-master1234.firebaseapp.com",
-  projectId: "promt-master1234",
-  storageBucket: "promt-master1234.firebasestorage.app",
-  messagingSenderId: "77863236164",
-  appId: "1:77863236164:web:754e9cf673890514a68211"
+    apiKey: "AIzaSyB2rnjkUqZ7zsya8A4-NXjuu8_V2kmJRfQ",
+    authDomain: "promt-master1234.firebaseapp.com",
+    projectId: "promt-master1234",
+
+    // 🔥 HIER WAR DER BUG
+    storageBucket: "promt-master1234.appspot.com",
+
+    messagingSenderId: "77863236164",
+    appId: "1:77863236164:web:754e9cf673890514a68211"
 };
 
 export const app = initializeApp(firebaseConfig);
